e254a4c37297fc315ef2adda27f4f4e20b0554f1
akshaybho <bhogale.akshay11@gmail.com>
2023-10-09 21:51:53 +0530

    basics

---
+++ Basic_1/Conditional_1.js
@@ -0,0 +1,5 @@
+let a = prompt("what is your age?")
+a = Number.parseInt(a)
+if(a>0){
+    alert("this is valid age")
+}
---
+++ Basic_1/Practice_1.js
@@ -0,0 +1,8 @@
+let age = "what is your age?";
+age = 15
+if(age>10 && age<20){
+    console.log("your age is lies between 10 and 20");
+}
+else{
+    console.log("your age doesnt lies between 10 and 20");
+}
