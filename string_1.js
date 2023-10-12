982b2969cda0c069252c090aee1541beb1cf9a2a
akshaybho <bhogale.akshay11@gmail.com>
2023-10-12 21:59:53 +0530

    learn string

---
+++ string_1.js
@@ -0,0 +1,8 @@
+let boy1 = "Pramod"
+let boy2 = "Nikhil"
+let sentence = `${boy1} is a friend of ${boy2}`
+console.log(sentence)
+
+let n = "Harry"
+console.log(n.slice(2, 4))
+console.log(n.replace("Har", "Per"))
---
+++ string_2.js
@@ -0,0 +1,4 @@
+let str = "please give Rs 1000"
+let amount = Number.parseInt(str.length(15))
+console(amount)
+
